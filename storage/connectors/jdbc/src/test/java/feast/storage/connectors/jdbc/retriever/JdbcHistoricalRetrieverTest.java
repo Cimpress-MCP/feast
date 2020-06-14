c9217fe97d333cc7f7bd53d59695e2e7c6d78954
chi zhang <chi.zhang@vistaprint.com>
2020-06-13 22:21:26 -0400

    mvn spotless

--- storage/connectors/jdbc/src/test/java/feast/storage/connectors/jdbc/retriever/JdbcHistoricalRetrieverTest.java
+++ storage/connectors/jdbc/src/test/java/feast/storage/connectors/jdbc/retriever/JdbcHistoricalRetrieverTest.java
@@ -16,19 +16,11 @@
  */
 package feast.storage.connectors.jdbc.retriever;
 
-import com.google.protobuf.Duration;
-import feast.proto.core.FeatureSetProto;
-import feast.proto.core.FeatureSetProto.EntitySpec;
-import feast.proto.serving.ServingAPIProto;
-import feast.storage.api.retriever.FeatureSetRequest;
 import feast.storage.api.retriever.HistoricalRetriever;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -52,59 +44,60 @@ public class JdbcHistoricalRetrieverTest {
   public void dummyTest() {
     Assert.assertEquals(1, 1);
   }
-//  @Before
-//  public void setUp() {
-//    postgressqlConfig.put("class_name", class_name);
-//    postgressqlConfig.put("username", username);
-//    postgressqlConfig.put("password", pw);
-//    postgressqlConfig.put("url", url);
-//    postgressqlConfig.put("staging_location", staging_location);
-//
-//    postgresqlFeatureRetriever = JdbcHistoricalRetriever.create(postgressqlConfig);
-//  }
-//
-//  @Test
-//  public void shouldRetrieveFromPostgresql() {
-//    //      Set CSV format DATA_FORMAT_CSV = 2; where the first column of the csv file must be the
-//    // entity_id
-//    //      file_uri is under
-//    // src/test/java/feast/storage/connectors/jdbc/retriever/myproject2_entity_rows.csv
-//    String file_uris = System.getenv("file_uri");
-//    ServingAPIProto.DatasetSource.FileSource fileSource =
-//        ServingAPIProto.DatasetSource.FileSource.newBuilder()
-//            .setDataFormatValue(2)
-//            .addFileUris(file_uris)
-//            .build();
-//
-//    ServingAPIProto.DatasetSource datasetSource =
-//        ServingAPIProto.DatasetSource.newBuilder().setFileSource(fileSource).build();
-//
-//    String retrievalId = "1234";
-//    FeatureSetRequest featureSetRequest =
-//        FeatureSetRequest.newBuilder()
-//            .setSpec(getFeatureSetSpec())
-//            .addFeatureReference(
-//                ServingAPIProto.FeatureReference.newBuilder()
-//                    .setName("feature_1")
-//                    .setProject("myproject2")
-//                    .setFeatureSet("feature_set")
-//                    .build())
-//            .build();
-//    List<FeatureSetRequest> featureSetRequests = new ArrayList<>();
-//    featureSetRequests.add(featureSetRequest);
-//
-//    System.out.println(datasetSource.getFileSource());
-//    postgresqlFeatureRetriever.getHistoricalFeatures(
-//        retrievalId, datasetSource, featureSetRequests);
-//  }
-//
-//  private FeatureSetProto.FeatureSetSpec getFeatureSetSpec() {
-//    return FeatureSetProto.FeatureSetSpec.newBuilder()
-//        .setProject("myproject2")
-//        .setName("feature_set")
-//        .addEntities(EntitySpec.newBuilder().setName("entity_id_primary"))
-//        .addFeatures(FeatureSetProto.FeatureSpec.newBuilder().setName("feature_1"))
-//        .setMaxAge(Duration.newBuilder().setSeconds(30)) // default
-//        .build();
-//  }
+  //  @Before
+  //  public void setUp() {
+  //    postgressqlConfig.put("class_name", class_name);
+  //    postgressqlConfig.put("username", username);
+  //    postgressqlConfig.put("password", pw);
+  //    postgressqlConfig.put("url", url);
+  //    postgressqlConfig.put("staging_location", staging_location);
+  //
+  //    postgresqlFeatureRetriever = JdbcHistoricalRetriever.create(postgressqlConfig);
+  //  }
+  //
+  //  @Test
+  //  public void shouldRetrieveFromPostgresql() {
+  //    //      Set CSV format DATA_FORMAT_CSV = 2; where the first column of the csv file must be
+  // the
+  //    // entity_id
+  //    //      file_uri is under
+  //    // src/test/java/feast/storage/connectors/jdbc/retriever/myproject2_entity_rows.csv
+  //    String file_uris = System.getenv("file_uri");
+  //    ServingAPIProto.DatasetSource.FileSource fileSource =
+  //        ServingAPIProto.DatasetSource.FileSource.newBuilder()
+  //            .setDataFormatValue(2)
+  //            .addFileUris(file_uris)
+  //            .build();
+  //
+  //    ServingAPIProto.DatasetSource datasetSource =
+  //        ServingAPIProto.DatasetSource.newBuilder().setFileSource(fileSource).build();
+  //
+  //    String retrievalId = "1234";
+  //    FeatureSetRequest featureSetRequest =
+  //        FeatureSetRequest.newBuilder()
+  //            .setSpec(getFeatureSetSpec())
+  //            .addFeatureReference(
+  //                ServingAPIProto.FeatureReference.newBuilder()
+  //                    .setName("feature_1")
+  //                    .setProject("myproject2")
+  //                    .setFeatureSet("feature_set")
+  //                    .build())
+  //            .build();
+  //    List<FeatureSetRequest> featureSetRequests = new ArrayList<>();
+  //    featureSetRequests.add(featureSetRequest);
+  //
+  //    System.out.println(datasetSource.getFileSource());
+  //    postgresqlFeatureRetriever.getHistoricalFeatures(
+  //        retrievalId, datasetSource, featureSetRequests);
+  //  }
+  //
+  //  private FeatureSetProto.FeatureSetSpec getFeatureSetSpec() {
+  //    return FeatureSetProto.FeatureSetSpec.newBuilder()
+  //        .setProject("myproject2")
+  //        .setName("feature_set")
+  //        .addEntities(EntitySpec.newBuilder().setName("entity_id_primary"))
+  //        .addFeatures(FeatureSetProto.FeatureSpec.newBuilder().setName("feature_1"))
+  //        .setMaxAge(Duration.newBuilder().setSeconds(30)) // default
+  //        .build();
+  //  }
 }
