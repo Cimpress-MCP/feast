93eff6211044ee7fb787b3360d8f5e700a2e0add
chi zhang <chi.zhang@vistaprint.com>
2020-07-14 11:53:57 -0400

    updated comments

--- storage/connectors/jdbc/src/main/java/feast/storage/connectors/jdbc/retriever/AbstractJdbcQueryTemplater.java
+++ storage/connectors/jdbc/src/main/java/feast/storage/connectors/jdbc/retriever/AbstractJdbcQueryTemplater.java
@@ -187,7 +187,6 @@ public abstract class AbstractJdbcQueryTemplater implements JdbcQueryTemplater {
           String.format("Could not parse staging location: %s", stagingLocation), e);
     }
     String stagingPath = stagingUri.getPath();
-    // TODO: need flexible file type
     String exportPath =
         String.format("%s/%s.%s", stagingPath.replaceAll("/$", ""), resultTable, EXPORT_FILE_FORMAT);
     List<String> exportTableSqlQueries = this.generateExportTableSqlQuery(resultTable, stagingPath);
@@ -200,8 +199,8 @@ public abstract class AbstractJdbcQueryTemplater implements JdbcQueryTemplater {
     } catch (SQLException e) {
       throw new RuntimeException(
           String.format(
-              "Could not export resulting historical dataset using query: \n%s",
-              exportTableSqlQueries),
+              "Could not export resulting historical dataset with data format: \n%s \n using query: \n%s",
+                  EXPORT_FILE_FORMAT, exportTableSqlQueries),
           e);
     }
   }
